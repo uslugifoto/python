fcf2344ca17618a40969991555c7733a377e152a
goorange@hackintosh <grzegorz.grondziel@gmail.com>
2016-08-02 15:55:00 +0200

    Wstęp do opisu typow danych w Pythonie.

---
+++ Książka/Przykłady/temp.py
@@ -0,0 +1,26 @@
+# Liczby
+
+Liczby_1 = 123
+Liczby_2 = 3.14
+#Liczby_3 =
+
+print(Liczby_1)
+print(Liczby_2)
+#print(Liczby_3)
+
+# Łańcuchy znaków
+
+Łańcuchy_znaków_1 = b'a\x01c'
+Łańcuchy_znaków_2 = 'mielonka'
+Łańcuchy_znaków_3 = "Grzegorz"
+
+print(Łańcuchy_znaków_1)
+print(Łańcuchy_znaków_2)
+print(Łańcuchy_znaków_3)
+
+# Listy
+
+Lista_1 = [1 ,2, 3]
+print(Lista_1)
+
+
