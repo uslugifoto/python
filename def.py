8eb49b1e58168607815474270321e134bc88ea6b
goorange@dell <grzegorz.grondziel@gmail.com>
2016-08-14 23:51:07 +0200

    Dodano def.py

---
+++ def.py
@@ -0,0 +1,14 @@
+def powiedz_czesc():
+    print("Cześć!")
+powiedz_czesc()
+
+def add(a, b, c):
+    return a + b + c
+x = add(1, 3, 5)
+print(x)
+
+def cyfry(*args):
+    print(args)
+    return sum(args) / len(args)
+x = cyfry(2, 2, 2, 2, 2)
+print(x)
