00671b0838fce32243124543f83467a4bb382532
goorange@surface <grzegorz.grondziel@gmail.com>
2016-08-15 22:32:38 +0200

    While, nowy plik.

---
+++ Kurs/while.py
@@ -0,0 +1,19 @@
+a = 10
+
+while a >= 1:
+    print(a)
+    a -= 1
+
+b = 1
+
+while b <= 10:
+    print(b)
+    b += 1
+
+c = 11
+
+while c <= 11:
+    print(c)
+    c -= 1
+else:
+    print('c jest inne niż 11')
