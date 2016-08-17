3cc6406dbad98714893e4bbdc591d9d904057c32
Grzegorz Grondziel <goorange@hamac>
2016-08-17 17:23:08 +0200

    Dodano folder "Moje_przyklady" a w nim plik typy_danych.py

---
+++ Moje_przyklady/typy_danych.py
@@ -0,0 +1,16 @@
+a = 1
+b = 1.1
+c = 'a'
+d = ('z')
+s = {'s'}
+
+x = "Abc"
+y = 3 + 4j
+v = [4, -1.5, "e"]
+z = 5 ** 43
+
+print(type(x))
+print(type(y))
+print(type(v))
+print(type(z))
+print(type(s))
